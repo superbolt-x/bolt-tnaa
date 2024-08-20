2ffd423d66532c2c0976ab2829582b44c798969e
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-08-20 11:50:39 -0400

    Update googleads_ad_performance.sql
    campaign_type_default

--- models/reporting/googleads_ad_performance.sql
+++ models/reporting/googleads_ad_performance.sql
@@ -8,7 +8,12 @@ ad_id,
 campaign_name,
 campaign_id,
 campaign_status,
-campaign_type_default,
+CASE WHEN campaign_name ~* 'Search' AND campaign_name ~* 'NB' THEN 'Campaign Type: Search Nonbrand'
+    WHEN campaign_name ~* 'Search' AND campaign_name !~* 'NB' THEN 'Campaign Type: Search Branded'
+    WHEN campaign_name ~* 'Performance Max' OR campaign_name ~* 'PMax' THEN 'Campaign Type: Performance Max'
+    WHEN campaign_name ~* 'DSA' THEN 'Campaign Type: DSA'
+    ELSE 'Campaign Type: Other'
+END as campaign_type_default,
 ad_group_name,
 ad_group_id,
 date,
