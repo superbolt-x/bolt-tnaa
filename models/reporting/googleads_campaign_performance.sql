73495f4d01f860d37fa64269b7abd1968d1cd73c
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-08-26 13:55:36 -0600

    Update googleads_campaign_performance.sql
    southport

--- models/reporting/googleads_campaign_performance.sql
+++ models/reporting/googleads_campaign_performance.sql
@@ -10,6 +10,7 @@ campaign_status,
 campaign_type_default,
 CASE WHEN campaign_name ~* 'NB' AND campaign_name !~* 'DSA' AND campaign_name !~* 'Allentown' THEN 'Unbrand - US'
     WHEN campaign_name ~* 'Allentown' THEN 'Unbrand - Allentown'
+    WHEN campaign_name ~* 'Southport' THEN 'Unbrand - Southport'
     WHEN campaign_name ~* 'Brand' THEN 'Brand'
     WHEN campaign_name ~* 'Performance Max' THEN 'Performance Max'
     WHEN campaign_name ~* 'DSA' THEN 'DSA'
