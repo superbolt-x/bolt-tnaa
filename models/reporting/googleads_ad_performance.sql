8fb9e7eb6ef225d7aa0d0ac824a2471d7df10b4d
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-10-07 16:01:00 -0600

    Update googleads_ad_performance.sql
    unbrand allied

--- models/reporting/googleads_ad_performance.sql
+++ models/reporting/googleads_ad_performance.sql
@@ -9,7 +9,8 @@ campaign_name,
 campaign_id,
 campaign_status,
 campaign_type_default,
-CASE WHEN campaign_name ~* 'NB' AND campaign_name !~* 'DSA' AND campaign_name !~* 'Allentown' THEN 'Unbrand - US'
+CASE WHEN campaign_name ~* 'NB' AND campaign_name ~* 'Allied' THEN 'Unbrand - Allied'
+    WHEN campaign_name ~* 'NB' AND campaign_name !~* 'DSA' AND campaign_name !~* 'Allentown' THEN 'Unbrand - US'
     WHEN campaign_name ~* 'Allentown' THEN 'Unbrand - Allentown'
     WHEN campaign_name ~* 'Southport' THEN 'Unbrand - Southport'
     WHEN campaign_name ~* 'Brand' THEN 'Brand'
